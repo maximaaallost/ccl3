a9d3e345ca7a8dd3f11934fc8de61538ab95022c
Venii24 <stefanieplankensteiner24@gmail.com>
2024-01-16 11:17:22 +0100

    added Color

--- app/src/main/java/com/cc221013/bookify/ui/theme/Color.kt
+++ app/src/main/java/com/cc221013/bookify/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.cc221013.bookify.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Yellow = Color(0xFFE4A53B)
+val NonWhite = Color(0xFFFBF6F3)
+val DarkBeige = Color(0xFFDCC6B4)
+val LightBeige = Color(0xFFF0E3D8)
+val Violet = Color(0xFF20002F)
+val LightViolet = Color(0xFF735D7A)
