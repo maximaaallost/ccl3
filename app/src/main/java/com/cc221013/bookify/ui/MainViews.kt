e148c539acd47e58925373ee94788e77f6c4bca1
Venii24 <stefanieplankensteiner24@gmail.com>
2024-01-17 17:07:31 +0100

    meged me stefanie into master

--- app/src/main/java/com/cc221013/bookify/ui/MainViews.kt
+++ app/src/main/java/com/cc221013/bookify/ui/MainViews.kt
@@ -34,7 +34,6 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.AlertDialog
