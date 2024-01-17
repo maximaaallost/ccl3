5e6c938230d41c0b214224357585ee4dd66385d8
maxima <cc221022@fhstp.ac.at>
2024-01-17 10:11:18 +0100

    navigationbar done

--- app/src/main/java/com/cc221013/bookify/ui/MainViews.kt
+++ app/src/main/java/com/cc221013/bookify/ui/MainViews.kt
@@ -6,6 +6,7 @@ import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -20,6 +21,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.BottomNavigation
@@ -50,8 +52,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -122,26 +127,65 @@ fun MainView(mainViewModel: MainViewModel){
     }
 }
 
+
+//NavigationBarItem -> adjust colors according to tab
+@Composable
+fun NavigationBarItem(
+    selected: Boolean,
+    onClick: () -> Unit,
+    icon: Painter, // Change the type to Painter
+    contentDescription: String
+) {
+    Box(
+        modifier = Modifier
+            .clickable(onClick = onClick)
+            .background(
+                color = if (selected) Yellow else Violet,
+                shape = RoundedCornerShape(50.dp)
+            )
+            .size(55.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        androidx.compose.material3.Icon(
+            painter = icon, // Use the painter property
+            contentDescription = contentDescription,
+            tint = NonWhite,
+            modifier = Modifier.size(32.dp)
+        )
+    }
+}
+
 @Composable
 fun BottomNavigationBar(navController: NavHostController, selectedScreen: Screen){
     BottomNavigation (
         backgroundColor = Violet,
         modifier = Modifier.height(70.dp)
+            ,
     ) {
-        NavigationBarItem(
-            selected = (selectedScreen == Screen.Read),
-            onClick = { navController.navigate(Screen.Read.route) },
-            icon = { Icon(imageVector = Icons.Default.Home, contentDescription = "") })
+        Row(modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            NavigationBarItem(
+                selected = (selectedScreen == Screen.Read),
+                onClick = { navController.navigate(Screen.Read.route) },
+                icon = painterResource(id = R.drawable.read),
+                contentDescription = "Read Books section"
+                )
+            NavigationBarItem(
+                selected = (selectedScreen == Screen.TBR),
+                onClick = { navController.navigate(Screen.TBR.route) },
+                icon = painterResource(id = R.drawable.tbr),
+                contentDescription = "To be Read Section"
+                )
 
-        NavigationBarItem(
-            selected = (selectedScreen == Screen.TBR),
-            onClick = { navController.navigate(Screen.TBR.route) },
-            icon = { Icon(imageVector = Icons.Default.AccountBox, contentDescription = "") })
-
-        NavigationBarItem(
-            selected = (selectedScreen == Screen.Wishlist),
-            onClick = { navController.navigate(Screen.Wishlist.route) },
-            icon = { Icon(imageVector = Icons.Default.AccountBox, contentDescription = "") })
+            NavigationBarItem(
+                selected = (selectedScreen == Screen.Wishlist),
+                onClick = { navController.navigate(Screen.Wishlist.route) },
+                icon = painterResource(id = R.drawable.wishlist),
+                contentDescription = "Wishlist"
+            )
+        }
     }
 }
 
